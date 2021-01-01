5532fd2f54835649b7a503aeea7d663805a3b493
Erick1501 <erickpc1501@gmail.com>
2020-12-31 19:31:28 -0500

    1

---
+++ src/pages/voluntariado.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import NavbarComponent from "../components/NavbarComponent";
+const Voluntariado = () => {
+    return(<main>
+        <NavbarComponent />
+        
+    </main>)
+} 
+export default Voluntariado 
